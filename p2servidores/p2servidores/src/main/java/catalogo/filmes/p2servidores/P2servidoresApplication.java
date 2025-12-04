fcef4de485f9fe61bdc1bb940522238c25146dce
LucasAmigoT <160610735+LucasAmigoT@users.noreply.github.com>
2025-12-04 20:31:59 -0300

    base projeto
    arquivo de download do https://start.spring.io/

---
+++ p2servidores/p2servidores/src/main/java/catalogo/filmes/p2servidores/P2servidoresApplication.java
@@ -0,0 +1,13 @@
+package catalogo.filmes.p2servidores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2servidoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2servidoresApplication.class, args);
+	}
+
+}
