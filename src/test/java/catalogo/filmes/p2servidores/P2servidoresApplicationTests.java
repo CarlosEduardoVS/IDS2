adf70dc8868916d264aedef664a305e8f95a8bd2
LucasAmigoT <160610735+LucasAmigoT@users.noreply.github.com>
2025-12-04 20:50:59 -0300

    base do sistema
    sistema https://start.spring.io/

---
+++ src/test/java/catalogo/filmes/p2servidores/P2servidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package catalogo.filmes.p2servidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2servidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
